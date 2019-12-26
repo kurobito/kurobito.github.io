69dc9469289de563b1958555ec7728f4decefa1f
Van <55959247+kurobito@users.noreply.github.com>
2019-12-26 10:30:32 +0100

    moved nysl to subfolder new project usa open gov finished home page added api call implemented search function

--- scripts/getCongressMembers.js
+++ scripts/getCongressMembers.js
@@ -1,54 +1,98 @@
 // ProPublica Congress API Info
 const apiKey = 'cHWA0cyVrIeIZHFmZyrlp3UBNFO1aqLn7LsYLij2';
-const url = 'https://api.propublica.org/congress/v1/116/senate/members.json';
+let url = 'https://api.propublica.org/congress/v1/116/{chamber}/members.json';
+// const url = 'https://api.propublica.org/congress/v1/116/senate/members.json';
 const congressHeaders = new Headers();
 congressHeaders.append('X-API-KEY', apiKey);
-let congressMembers;
+let houseMembers;
+let senateMembers;
+import {setNavBarLogic} from './navbar.js';
+url = setNavBarLogic(url);
+const chamber = window.location.href.split('?')[1];
 
-const getCongressData = async () =>{
-	if (!congressMembers) {
-		const request = new Request(url, {method: 'GET', headers: congressHeaders});
-		try{
-			const response = await fetch(request);
-			if(response.ok){
-				const jsonResponse = await response.json();
-				congressMembers = jsonResponse.results[0].members;
-				console.log(congressMembers);
-				updateVue(congressMembers);
+
+// fetch request
+const getCongressData = async (chamber) =>{
+	const request = new Request(url, {method: 'GET', headers: congressHeaders});
+	try{
+		const response = await fetch(request);
+		if(response.ok){
+			const jsonResponse = await response.json();
+			console.log(jsonResponse);
+			switch(chamber){
+				case 'senate':
+				senateMembers = jsonResponse.results[0].members;
+				return senateMembers;
+				case 'house':
+				houseMembers = jsonResponse.results[0].members;
+				return houseMembers;
 			}
-		}catch(e){console.log(e)}
-	}
+		}else throw new Error(`${response.status} : ${response.statusText}`)
+	}catch(e){console.log(e)}
 }
 
+// create Vue instance
 const app = new Vue({
 	el: '#app',
 	data: {
 		message: 'Hello Vue!',
-		congressMembers: [],
+		houseMembers: [],
+		senateMembers: [],
 		searchQuery: ''
 	}
 })
 
-Vue.component('congress-member-card', {
-	props: ['member'],
-	template: `<div class="card" style="width: 18rem;">
-	<a v-bind:href="member.url"><h5 class="card-header"> {{ member.first_name }} {{ member.middle_name }} {{ member.last_name }}</h5></a>
+// Create component to display a senate member
+Vue.component('senate-member-card', {
+	props: ['senateMember'],
+	template: `<div class="col-sm-6 col-lg-3">
+	<div class="card" style="width: 16rem;">
+	<a v-bind:href="senateMember.url"><h5 class="card-header"> {{ senateMember.first_name }} {{ senateMember.middle_name }} {{ senateMember.last_name }}</h5></a>
 	<ul class="list-group list-group-flush">
-	<li class="list-group-item party" v-if="member.party === 'R'">Republican</li>
-	<li class="list-group-item party" v-if="member.party === 'D'">Democrat</li>
-	<li class="list-group-item party" v-if="member.party === 'ID'">Independent</li>
-	<li class="list-group-item state">State: {{ member.state }}</li>
-	<li class="list-group-item seniority">Seniority: {{ member.seniority }}</li>
-	<li class="list-group-item party-votes">Party Votes with: {{ member.votes_with_party_pct }}%</li>
-	<li class="list-group-item party-votes">Party Votes against: {{ member.votes_against_party_pct }}%</li>
+	<li class="list-group-item party" v-if="senateMember.party === 'R'">Republican</li>
+	<li class="list-group-item party" v-else-if="senateMember.party === 'D'">Democrat</li>
+	<li class="list-group-item party" v-else-if="senateMember.party === 'ID'">Independent</li>
+	<li class="list-group-item party" v-else>Other</li>
+	<li class="list-group-item state">State: {{ senateMember.state }}</li>
+	<li class="list-group-item seniority">Seniority: {{ senateMember.seniority }}</li>
+	<li class="list-group-item party-votes">Party Votes with: {{ senateMember.votes_with_party_pct }}%</li>
+	<li class="list-group-item party-votes">Party Votes against: {{ senateMember.votes_against_party_pct }}%</li>
 	</ul>
+	</div>
 	</div>`
 })
 
-function updateVue(congressMembers){
-	app.congressMembers = congressMembers;
-}
+// Create component to display a house member
+Vue.component('house-member-card', {
+	props: ['houseMember'],
+	template: `<div class="col-sm-6 col-lg-3">
+	<div class="card" style="width: 16rem;">
+	<a v-bind:href="houseMember.url"><h5 class="card-header"> {{ houseMember.first_name }} {{ houseMember.middle_name }} {{ houseMember.last_name }}</h5></a>
+	<ul class="list-group list-group-flush">
+	<li class="list-group-item party" v-if="houseMember.party === 'R'">Republican</li>
+	<li class="list-group-item party" v-else-if="houseMember.party === 'D'">Democrat</li>
+	<li class="list-group-item party" v-else-if="houseMember.party === 'ID'">Independent</li>
+	<li class="list-group-item party" v-else>Other</li>
+	<li class="list-group-item state">State: {{ houseMember.state }}</li>
+	<li class="list-group-item seniority">Seniority: {{ houseMember.seniority }}</li>
+	<li class="list-group-item party-votes">Party Votes with: {{ houseMember.votes_with_party_pct }}%</li>
+	<li class="list-group-item party-votes">Party Votes against: {{ houseMember.votes_against_party_pct }}%</li>
+	</ul>
+	</div>
+	</div>`
+})
 
+// update vue app
+function updateVue(member){
+	switch(chamber){
+		case 'senate':
+		app.senateMembers = member;
+		break;
+		case 'house':
+		app.houseMembers = member;
+		break;
+	}
+}
 function searchCongressMembers(congressMembers, searchQuery){
 	let searchedCongressMembers = congressMembers.filter(member => {
 		// if(member.first_name.toLowerCase().includes(searchQuery.toLowerCase())) {
@@ -61,10 +105,79 @@ function searchCongressMembers(congressMembers, searchQuery){
 	updateVue(searchedCongressMembers);
 }
 
-getCongressData();
+function filterCongressMembersByParty(partyFilterList, members){
+	let filteredCongressMembers = members.filter(member => {
+		for(let i = 0; i < partyFilterList.length; i++){
+			// console.log(`${partyFilterList[i]} === ${member.party} = ${partyFilterList[i] === member.party}`)
+			if (partyFilterList[i] === member.party) return member;
+		}
+		if (member.party != 'R' || member.party != 'D' || member.party != 'ID') {
+			console.log(member);
+		}
+	})
+	console.log(filteredCongressMembers);
+	updateVue(filteredCongressMembers);
+}
+
+// eventListener for search
 let searchByNameInput = document.getElementById('searchByName');
 searchByNameInput.oninput = () =>{
 	if(congressMembers){
 		searchCongressMembers(congressMembers, searchByNameInput.value);
 	}
 }
+
+// eventListener for filter by party
+let filterByParty = document.forms['filterByParty'].elements['partyFilters[]'];
+let filterAll = document.getElementById('filterAll');
+let partyFilterList = [];
+
+filterAll.onchange = () => {
+	if (filterAll.checked) {
+		partyFilterList.splice(0); // makes sure the array is always empty before adding all filters
+		filterByParty.forEach(partyFilter => {
+			partyFilter.checked = true;
+			partyFilterList.push(partyFilter.value);
+		});
+	}else{
+		filterByParty.forEach(partyFilter => {
+			partyFilter.checked = false;
+		});
+		partyFilterList.splice(0); 
+	} 
+
+	console.log(partyFilterList);
+	if (senateMembers) {
+		filterCongressMembersByParty(partyFilterList, senateMembers);
+	}else{
+		filterCongressMembersByParty(partyFilterList, houseMembers);
+	}
+}
+filterByParty.forEach(partyFilter => {
+	partyFilter.onchange = () => {
+		if(partyFilter.checked){
+			if (partyFilterList.length < filterByParty.length) {
+				partyFilterList.push(partyFilter.value);
+			}
+		} else partyFilterList.splice(partyFilterList.indexOf(partyFilter.value), 1);
+		if (partyFilterList.length === filterByParty.length) filterAll.checked = true;
+		else filterAll.checked = false;
+		console.log(partyFilterList);
+		if (senateMembers) {
+			filterCongressMembersByParty(partyFilterList, senateMembers);
+		}else{
+			filterCongressMembersByParty(partyFilterList, houseMembers);
+		}
+	}
+})
+
+const init = () => {
+	getCongressData(chamber).then(members => {
+		updateVue(members);
+		filterAll.onchange()
+	});
+}
+
+setTimeout(init, 500);
+
+
---
+++ scripts/navbar.js
@@ -0,0 +1,26 @@
+const congressListItem = document.getElementById('congressListItem');
+const congressSenate = document.getElementById('congressSenate');
+const congressHouse = document.getElementById('congressHouse');
+const attendanceListItem = document.getElementById('attendanceListItem');
+const attendanceSenate = document.getElementById('attendanceSenate');
+const aatendanceHouse = document.getElementById('attendanceHouse');
+const partyLoyaltyListItem = document.getElementById('partyLoyaltyListItem');
+const partyLoyaltySenate = document.getElementById('partyLoyaltySenate');
+const partyLoyaltyHouse = document.getElementById('partyLoyaltyHouse');
+
+// let url = 'https://api.propublica.org/congress/v1/116/{chamber}/members.json';
+
+const setNavBarLogic = url => {
+	console.log(window.location.href.split('?'))
+	const chamber = window.location.href.split('?')[1];
+	switch(chamber){
+		case 'senate':
+			url = url.replace('{chamber}', 'senate');
+			return url;
+		case 'house':
+			url = url.replace('{chamber}', 'house');
+			return url;
+	}
+}
+
+export {setNavBarLogic};
